93c0e32774b4d4876f98a3d0489c3c74f9cc8770
David Ramiro Barragan <dramirobarragan@gmail.com>
2020-05-01 10:12:29 +0200

    añadido google analitics

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -59,5 +59,34 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'UA-112055111-2',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**', '/do-not-track/me/too/'],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: 'GTM-PR4NNZJ',
+        // Enables Google Optimize Experiment ID
+        experimentId: 'v6vJtt3KR5SaeG6L6sQz8Q',
+        // Set Variation ID. 0 for original 1,2,3....
+        //variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: 'davidramiro.com',
+      },
+    },
   ],
 };

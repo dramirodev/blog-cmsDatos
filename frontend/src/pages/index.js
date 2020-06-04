09f6845f55b871f26d4d90a2e78930b11c421d23
David Ramiro Barragan <dramirobarragan@gmail.com>
2020-06-04 13:05:23 +0200

    modificando los imports

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout';
-import SEO from '../components/seo';
+import SEO from '../components/Seo';
 import InfoUsuario from '../components/InfoUsuario';
 import {useStaticQuery, graphql} from 'gatsby';
 
